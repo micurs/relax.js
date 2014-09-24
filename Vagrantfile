b35ce4b145f71c9d2072f3bb82082639a811af1c
Michele Ursino <mursino@amilink.com>
2014-09-24 16:23:04 -0400

    Add non-privileged init.sh to initialize application modules
    Signed-off-by: Michele Ursino <mursino@amilink.com>

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,9 @@ Vagrant.configure("2") do |config|
     v.memory = 1024
     v.cpus = 4
   end
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
 
-  config.vm.provision :shell, :path => "setup.sh"
   config.vm.synced_folder ".", "/micurs.com"
+  config.vm.provision :shell, :path => "setup.sh",  privileged: true
+  config.vm.provision :shell, :path => "init.sh",  privileged: false
 end
---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "========== Initialize micurs.com ==============="
+
+function gulp_install {
+    echo " - Node install: $1"
+    npm install $1 --save-dev  > /dev/null
+}
+
+echo "- Restore/Update all type definitions in typings"
+cd /micurs.com
+tsd reinstall --save --overwrite
+
+echo "- Npm install packages in package.json"
+cd site
+gulp_install gulp-typescript-compiler
+gulp_install gulp-less
+gulp_install gulp-uglify
+gulp_install gulp-rename
+gulp_install gulp-watch
+gulp_install gulp-minify-css
+npm install
