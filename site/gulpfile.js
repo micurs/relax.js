97cb21b186afc7d785216a510f94557689f23777
Michele Ursino <mursino@amilink.com>
2014-09-25 17:04:58 -0400

    replacing gulp-typescript-compiler with gulp-tsc
    Signed-off-by: Michele Ursino <mursino@amilink.com>

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -2,21 +2,22 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var print = require('gulp-print');
-var tsc  = require('gulp-typescript-compiler');
+var tsc  = require('gulp-tsc');
 
 
 gulp.task('ts_server', function() {
   return gulp.src( [ '*.ts', '**/*.ts', '!./public/**/*.ts', '!./node_modules/**/*.ts' ] )
-             .pipe( print() )
-             .pipe( tsc( {  module: 'amd', target: 'ES5', sourcemap: false, logErrors: true } ) )
-             .pipe(uglify());
+          .pipe( print(function(filepath) { return "TSC server side: " + filepath; } ) )
+          .pipe( tsc( {  module: 'amd', target: 'ES5', sourcemap: false, emitError: false } ) )
+          .pipe(gulp.dest('./'))
+          .pipe( print(function(filepath) { return "Result: " + filepath; } ) );
 });
 
 gulp.task('ts_client', function() {
   return gulp.src( [ './public/**/*.ts' ] )
-             .pipe( print() )
-             .pipe( tsc( {  module: 'amd', target: 'ES5', sourcemap: false, logErrors: true } ) )
-             .pipe(uglify());
+          .pipe( print( function(filepath) { return "Tsc client side: " + filepath; } ) )
+          .pipe( tsc( {  module: 'amd', target: 'ES5', sourcemap: true, emitError: false } ) )
+          .pipe(gulp.dest('./'));
 });
 
 
