c61309e025ec7510483979215dce9d54e917efec
Michele Ursino <mursino@amilink.com>
2014-10-09 18:35:05 -0400

    Compiled new app using relaxes module
    Signed-off-by: Michele Ursino <mursino@amilink.com>

--- site/bin/app.js
+++ site/bin/app.js
@@ -1,16 +1,19 @@
-///<reference path='../../typings/node/node.d.ts' />
-/* ///<reference path='../../relaxjs/bin/relaxjs.d.ts' /> */
-// App specific module
-// import relax = require("./../../relaxjs/src/relaxjs");
+///<reference path='./../../typings/node/node.d.ts' />
+///<reference path='./../../typings/underscore/underscore.d.ts' />
+///<reference path='./../../typings/underscore.string/underscore.string.d.ts' />
+///<reference path='./../../typings/q/Q.d.ts' />
+///<reference path='./../../typings/mime/mime.d.ts' />
+var relaxjs = require("./relaxjs");
+
 //import app = require("./application");
 //import controller = require("./controller"); // routing functions
 //import app = require("./micurs_com"); // specific resources for this site
 var portNumber = 3000;
 
 // Create the application by assembling the resources
-var site = app.Resources.Site.$('micurs.com');
-site.addResource(new app.Resources.HtmlView('home', 'layout'));
-site.addResource(new app.Resources.Data('resume'));
+var site = relaxjs.Site.$('micurs.com');
+site.addResource(new relaxjs.Resources.HtmlView('home', 'layout'));
+site.addResource(new relaxjs.Resources.Data('resume'));
 
 // Serve the app on the network
 var appSrv = site.serve();
