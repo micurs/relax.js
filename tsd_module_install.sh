c03a9f4f803b0b5737a1d29598f99d686e02b353
Michele Ursino <michele@micurs.com>
2014-10-10 10:37:40 -0400

    TSD commands updated

--- tsd_module_install.sh
+++ tsd_module_install.sh
@@ -3,4 +3,6 @@ tsd query bootstrap -s -a install
 tsd query moment -s -a install
 tsd query jquery -s -a install
 tsd query q -s -a install
-tsd mime q -s -a install
+tsd query mime -s -a install
+tsd query chalk -s -a install
+tsd query node -s -a install
